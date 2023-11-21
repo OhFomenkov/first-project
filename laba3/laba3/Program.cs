2caaa4c5657f17d8c5084624bcad6c95778d46e4
Ohtioma <artem.fomenkov04@mail.ru>
2023-11-21 20:02:06 +0300

    Добавление лаб

---
+++ laba3/laba3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace labfile
+{
+    class Class1
+    {
+        static void Writing(StreamWriter writer, string a1, string a2, StringBuilder temp, int digit)
+        {
+            string strdigit = digit.ToString("D4");
+            
+            writer.WriteLine("{0} {1} --> {2} {3}", a1, a2, temp, strdigit);
+        }
+        
+        
+        
+        
+        static void Reading()
+        {
+            StreamWriter writer = new("C:\\Users\\user\\Downloads\\output_1 (1).txt");
+            StreamReader reader = new("C:\\Users\\user\\Downloads\\input_1 (1).txt");
+            reader.ReadLine();//plate numbers
+            string str = reader.ReadLine();
+            (string, int) tuple;
+            int count = 0;
+            while (str != null)
+            {
+                if (!Regex.IsMatch(str, "[A-Z]{3}\\s\\d{4}"))
+                {
+                    writer.WriteLine(str + "--> не соответсвует формату входных данных");
+                    count++;
+                }
+                else
+                {
+                    count++;
+                    tuple = (str.Split()[0], Convert.ToInt32(str.Split()[1]));
+                    Preobr(writer, tuple);
+                }
+                str = reader.ReadLine();
+            }
+            writer.Close();
+            reader.Close();
+        }
+
+        static void Preobr(StreamWriter writer, (string, int) tuple)
+        {
+            string a1 = tuple.Item1;
+            string a2 = tuple.Item2.ToString("D4");
+            StringBuilder temp = new StringBuilder(tuple.Item1);
+            int digit = Convert.ToInt32(tuple.Item2);
+            if (digit == 9999 && temp[0] == 'Z' && temp[1] == 'Z' && temp[2] == 'Z')
+            {
+                writer.WriteLine("{0} {1} --> The last one", a1, a2);
+            }
+            if (digit != 9999)
+            {
+                digit++;
+                Writing(writer, a1, a2, temp, digit);
+            }
+            else
+            {
+                if (temp[2] == 'Z' && temp[1] == 'Z' && temp[0] != 'Z')
+                {
+                    temp[0] = Convert.ToChar(Convert.ToInt32(temp[0]) + 1);
+                    temp[1] = 'A';
+                    temp[2] = 'A';
+                    Writing(writer, a1, a2, temp, digit);
+
+                }
+                else if (temp[2] == 'Z' && temp[1] != 'Z')
+                {
+                    temp[1] = Convert.ToChar(Convert.ToInt32(temp[1]) + 1);
+                    temp[2] = 'A';
+                    Writing(writer, a1, a2, temp, digit);
+                }
+                else if (temp[2] != 'Z')
+                {
+                    temp[2] = Convert.ToChar(Convert.ToInt32(temp[2]) + 1);
+                    Writing(writer, a1, a2, temp, digit);
+                }
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Reading();
+        }
+    }
+}
---
+++ usr/usr/Class1.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+
+namespace usr;
+public class Class1
+{
+    private int[] IntArray;
+    private int n;
+    public int N => n;
+    public int Mult
+    {
+        set
+        {
+            for (int i = 0; i < IntArray.Length; i++)
+                IntArray[i] *= value;
+        }
+    }
+
+    public int Max => IntArray.Max();
+    public int Min => IntArray.Min();
+    public int Sum => IntArray.Sum();
+
+    public int this[int i]
+    {
+        get => IntArray[i];
+        set => IntArray[i] = value;
+    }
+
+    public Class1(int n)
+    {
+        this.n = n;
+        IntArray = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            IntArray[i] = 0;
+        }
+    }
+    
+    public Class1(int a, int b, int n)
+    {
+        Random rnd = new Random();
+        this.n = n;
+        IntArray = new int[n];
+        for(int j = 0; j < n; j++)
+        {
+            IntArray[j] = rnd.Next(a, b+1);
+        }
+    }
+
+    public Class1(Class1 arr, int a, int b)
+    {
+        this.n = b-a;
+        IntArray = new int[n+1];
+        for (int j = a; j < b; j++)
+        {
+            IntArray[j] = j;
+        }
+    }
+
+
+    public static Class1 operator ++(Class1 arr)
+    {
+        for (int i = 0; i < arr.N; i++)
+        {
+            arr[i]++;
+        }
+
+        return arr;
+    }
+    public static Class1 operator --(Class1 arr)
+    {
+        for (int i = 0; i < arr.N; i++)
+        {
+            arr[i]--;
+        }
+
+        return arr;
+    }
+
+    public static bool operator !(Class1 arr)
+    {
+        bool isUp = true;
+        for (int i = 1; i < arr.N; i++)
+        {
+            if (arr[i] < arr[i - 1])
+            {
+                isUp = false;
+            }
+        }
+        return isUp;
+    }
+    public static Class1 operator *(Class1 arr, int sk)
+    {
+        for (int i = 0; i < arr.N; i++)
+        {
+            arr[i] *= sk;
+        }
+
+        return arr;
+    }
+
+    public static explicit operator int[](Class1 arr)
+    {
+        return arr.IntArray;
+    }
+    public static explicit operator Class1(int[] arr)
+    {
+        var ar = new Class1(arr.Length);
+        ar.IntArray = arr;
+        return ar;
+    }
+
+    public void InputArr()
+    {
+        for (int i = 0; i < IntArray.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива: ");
+            this[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    public void OutArr()
+    {
+        for (int i = 0; i < IntArray.Length; i++)
+        {
+            Console.Write("{0} ", IntArray[i]);
+        }
+    }
+
+    public void BubbleSort()
+    {
+        int temp = 0;
+        for (int i = 0; i < IntArray.Length; i++)
+        {
+            for (int j = 0; j < IntArray.Length - 1; j++)
+            {
+                if (IntArray[j] > IntArray[j + 1])
+                {
+                    temp = IntArray[j + 1];
+                    IntArray[j + 1] = IntArray[j];
+                    IntArray[j] = temp;
+                }
+            }
+        }
+    }
+
+
+}
+    
